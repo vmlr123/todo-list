1667d80400e88eff5fc164700f54e7c59accf820
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2025-07-27 13:46:43 -0400

    added TodoListItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+import { useState } from 'react';
 
 function App() {
+  const [newTodo, setNewTodo] = useState('Example Text');
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
