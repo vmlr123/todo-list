ae2470b5c1ee6d529935496155cc25bcec4823d3
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2025-08-03 22:38:40 -0400

    form can add items to list of todos

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,9 @@
 import TodoListItem from './TodoListItem';
 
-function TodoList() {
-  const todos = [
-    { id: 1, title: 'review resources' },
-    { id: 2, title: 'take notes' },
-    { id: 3, title: 'code out app' },
-  ];
+function TodoList({ todoList }) {
   return (
     <ul>
-      {todos.map((todo) => (
+      {todoList.map((todo) => (
         <TodoListItem key={todo.id} todo={todo} />
       ))}
     </ul>
