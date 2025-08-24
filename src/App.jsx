8c1a94daac5a7a8889a2ee9df22c42dd0ee2a9c3
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2025-08-24 11:13:04 -0400

    connected form to Airtable base. loads and uploads saved todos from and to Airtable, displays pending messages, and error messages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,90 @@
 import './App.css';
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoList/TodoForm';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-  function addTodo(title) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isSaving, setIsSaving] = useState(false);
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+      const options = {
+        method: 'GET',
+        headers: { Authorization: token },
+      };
+      try {
+        const resp = await fetch(url, options);
+        if (!resp.ok) {
+          throw new Error(resp.message || 'Request failed');
+        }
+        const { records } = await resp.json();
+        setTodoList(
+          records.map((record) => {
+            const todo = {
+              id: record.id,
+              ...record.fields,
+            };
+            if (!todo.isCompleted) {
+              todo.isCompleted = false;
+            }
+            return todo;
+          })
+        );
+      } catch (error) {
+        console.error(error.message);
+        setErrorMessage(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
+  async function addTodo(title) {
+    const payload = { records: [{ fields: { title: title } }] };
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
     const newTodo = {
       id: Date.now(),
       title: title,
       isCompleted: false,
     };
-    setTodoList([...todoList, newTodo]);
+    try {
+      setIsSaving(true);
+      const resp = await fetch(url, options);
+      if (!resp.ok) {
+        throw new Error(resp.message || 'Request failed');
+      }
+      const { records } = await resp.json();
+      const savedTodo = {
+        id: records[0].id,
+        ...records[0].fields,
+      };
+      if (!savedTodo.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+      setTodoList([...todoList, savedTodo]);
+    } catch (error) {
+      console.error(error.message);
+      setErrorMessage(error.message);
+    } finally {
+      setIsSaving(false);
+    }
   }
-  function completeTodo(id) {
+  async function completeTodo(id) {
+    setIsSaving(true);
+    const originalTodo = todoList.find((todo) => todo.id === id);
     const updatedTodos = todoList.map((todo) => {
       if (todo.id === id) {
         return {
@@ -24,9 +95,44 @@ function App() {
         return todo;
       }
     });
-    setTodoList(updatedTodos);
+    setTodoList([...updatedTodos]);
+    const payload = {
+      records: [
+        {
+          id: originalTodo.id,
+          fields: {
+            title: originalTodo.title,
+            isCompleted: true,
+          },
+        },
+      ],
+    };
+    const options = {
+      method: 'PATCH',
+      headers: { Authorization: token, 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    };
+    try {
+      const resp = await fetch(url, options);
+      if (!resp.ok) {
+        throw new Error(resp.message || 'Request failed');
+      }
+    } catch (error) {
+      console.error(error.message);
+      setErrorMessage(`${error.message}. Reverting todo...`);
+      const revertedTodos = todoList.map((todo) => {
+        if (todo.id === originalTodo.id) {
+          return { ...originalTodo };
+        } else {
+          return todo;
+        }
+      });
+      setTodoList([...revertedTodos]);
+    } finally {
+      setIsSaving(false);
+    }
   }
-  function updateTodo(editedTodo) {
+  async function updateTodo(editedTodo) {
     const updatedTodos = todoList.map((todo) => {
       if (todo.id === editedTodo.id) {
         return { ...editedTodo };
@@ -34,17 +140,61 @@ function App() {
         return todo;
       }
     });
-    setTodoList(updatedTodos);
+    setTodoList([...updatedTodos]);
+    const originalTodo = todoList.find((todo) => todo.id === editedTodo.id);
+    const payload = {
+      records: [
+        {
+          id: editedTodo.id,
+          fields: {
+            title: editedTodo.title,
+            isCompleted: editedTodo.isCompleted,
+          },
+        },
+      ],
+    };
+    const options = {
+      method: 'PATCH',
+      headers: { Authorization: token, 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload),
+    };
+    try {
+      const resp = await fetch(url, options);
+      if (!resp.ok) {
+        throw new Error(resp.message || 'Request failed');
+      }
+    } catch (error) {
+      console.error(error.message);
+      setErrorMessage(`${error.message}. Reverting todo...`);
+      const revertedTodos = todoList.map((todo) => {
+        if (todo.id === originalTodo.id) {
+          return { ...originalTodo };
+        } else {
+          return todo;
+        }
+      });
+      setTodoList([...revertedTodos]);
+    } finally {
+      setIsSaving(false);
+    }
   }
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo} />
+      <TodoForm onAddTodo={addTodo} isSaving={isSaving} />
       <TodoList
         todoList={todoList}
         onCompleteTodo={completeTodo}
         onUpdateTodo={updateTodo}
+        isLoading={isLoading}
       />
+      {errorMessage !== '' && (
+        <div>
+          <hr />
+          <p>{errorMessage}</p>
+          <button onClick={() => setErrorMessage('')}>Dismiss</button>
+        </div>
+      )}
     </div>
   );
 }
