535dd2c0a5ca06e1f08b0c5aba340ebd4baffe10
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2025-09-22 00:56:30 -0400

    refactored to have a reducer hook that replaces 4 different states".

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,45 +1,88 @@
-const initialState = {
-    todoList: [],
-    isLoading: false,
-    isSaving: false,
-    errorMessage: '',
+export const initialState = {
+  todoList: [],
+  isLoading: false,
+  isSaving: false,
+  errorMessage: '',
 };
 
 export const actions = {
-    fetchTodos: 'fetchTodos',
-    loadTodos: 'loadTodos',
-    setLoadError: 'setLoadError',
-    startRequest: 'startRequest',
-    addTodo: 'addTodo',
-    endRequest: 'endRequest',
-    updateTodo: 'updateTodo',
-    completeTodo: 'completeTodo',
-    revertTodo: 'revertTodo',
-    clearError: 'clearError'
-}
+  fetchTodos: 'fetchTodos',
+  loadTodos: 'loadTodos',
+  setLoadError: 'setLoadError',
+  startRequest: 'startRequest',
+  addTodo: 'addTodo',
+  endRequest: 'endRequest',
+  updateTodo: 'updateTodo',
+  completeTodo: 'completeTodo',
+  revertTodo: 'revertTodo',
+  clearError: 'clearError',
+};
 
-function reducer(state = initialState, action) {
-    switch (action.type) {
-        case actions.fetchTodos:
-            return { ... state}
-        case actions.loadTodos:
-            return { ...state}
-        case actions.setLoadError:
-            return { ...state}
-        case actions.startRequest:
-            return { ...state}
-        case actions.addTodo:
-            return { ...state}
-        case actions.endRequest:
-            return { ...state}
-        case actions.updateTodo:
-            return { ...state}
-        case actions.completeTodo:
-            return { ...state}
-        case actions.revertTodo:
-            return { ...state}
-        case actions.clearError:
-            return { ...state}
-}
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.fetchTodos:
+      return { ...state, isLoading: true };
+    case actions.loadTodos:
+      const mappedArray = action.records.map((record) => {
+        const todo = {
+          id: record.id,
+          ...record.fields,
+        };
+        if (!todo.isCompleted) {
+          todo.isCompleted = false;
+        }
+        return todo;
+      });
+      return { ...state, todoList: mappedArray, isLoading: false };
+    case actions.setLoadError:
+      return { ...state, errorMessage: action.errorMessage, isLoading: false };
+    case actions.startRequest:
+      return { ...state, isSaving: true };
+    case actions.addTodo:
+      const savedTodo = {
+        id: action.records[0].id,
+        ...action.records[0].fields,
+      };
+      if (!savedTodo.isCompleted) {
+        savedTodo.isCompleted = false;
+      }
+      return {
+        ...state,
+        todoList: [...state.todoList, savedTodo],
+        isSaving: false,
+      };
+    case actions.endRequest:
+      return { ...state, isLoading: false, isSaving: false };
+    case actions.revertTodo:
+      action.editedTodo = action.originalTodo;
+    case actions.updateTodo:
+      const updatedTodos = state.todoList.map((todo) => {
+        if (todo.id === action.editedTodo.id) {
+          return { ...action.editedTodo };
+        } else {
+          return todo;
+        }
+      });
+      const updatedState = { ...state, todoList: [...updatedTodos] };
+      if (action.error) {
+        updatedTodos.errorMessage = action.errorMessage;
+      }
+      return updatedState;
+    case actions.completeTodo:
+      const updatedTodoList = state.todoList.map((todo) => {
+        if (todo.id === action.id) {
+          return {
+            ...todo,
+            isCompleted: true,
+          };
+        } else {
+          return todo;
+        }
+      });
+      return { ...state, todoList: [...updatedTodoList] };
 
+    case actions.clearError:
+      return { ...state, errorMessage: '' };
+  }
+}
 export default initialState;
