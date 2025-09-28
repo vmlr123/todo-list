b42c75bef08b3afe6d19c25f99dd9e2f61050c32
vmlr123 <107290237+vmlr123@users.noreply.github.com>
2025-09-28 19:59:44 -0400

    refactored, added routing and pagination.

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,25 +1,77 @@
 import TodoListItem from './TodoListItem';
 import styles from './TodoList.module.css';
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
   const filteredTodoList = todoList.filter(
     (todo) => todo.isCompleted === false
   );
-  return isLoading ? (
-    <p>Todo list loading...</p>
-  ) : filteredTodoList.length === 0 ? (
-    <p>Add todo above to get started</p>
-  ) : (
-    <ul className={styles.list}>
-      {filteredTodoList.map((todo) => (
-        <TodoListItem
-          key={todo.id}
-          todo={todo}
-          onCompleteTodo={onCompleteTodo}
-          onUpdateTodo={onUpdateTodo}
-        />
-      ))}
-    </ul>
+  const [searchParams, setSearchParams] = useSearchParams();
+  const itemsPerPage = 15;
+  const currentPage = parseInt(searchParams.get('page') || '1', 10);
+  const indexOfFirstTodo = (currentPage - 1) * itemsPerPage;
+  const totalPages = Math.ceil(filteredTodoList.length / itemsPerPage);
+  const pagedTodos = filteredTodoList.slice(
+    indexOfFirstTodo,
+    indexOfFirstTodo + itemsPerPage
+  );
+  const navigate = useNavigate();
+  function handlePreviousPage() {
+    setSearchParams({ page: String(Math.max(currentPage - 1, 1)) });
+  }
+  function handleNextPage() {
+    setSearchParams({ page: String(Math.min(currentPage + 1, totalPages)) });
+  }
+  useEffect(() => {
+    if (totalPages > 0) {
+      if (
+        typeof currentPage !== 'number' ||
+        currentPage < 1 ||
+        currentPage > totalPages
+      ) {
+        navigate('/');
+      }
+    }
+  }, [currentPage, totalPages, navigate]);
+  return (
+    <>
+      {isLoading ? (
+        <p>Todo list loading...</p>
+      ) : filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+      ) : (
+        <>
+          <ul className={styles.list}>
+            {pagedTodos.map((todo) => (
+              <TodoListItem
+                key={todo.id}
+                todo={todo}
+                onCompleteTodo={onCompleteTodo}
+                onUpdateTodo={onUpdateTodo}
+              />
+            ))}
+          </ul>
+          <div className={styles.paginationControls}>
+            <button
+              onClick={() => handlePreviousPage()}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+            <span>
+              Page {currentPage} of {totalPages}
+            </span>
+            <button
+              onClick={() => handleNextPage()}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
